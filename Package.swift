1c5ddd92b7af984baf9345cf2e6f28e8e97d3edc
Aurelius Prochazka <aurephd@gmail.com>
2022-06-07 20:10:09 -0700

    Added MIDIKit dependency

--- Package.swift
+++ Package.swift
@@ -6,8 +6,9 @@ let package = Package(
     name: "AudioKit",
     platforms: [.macOS(.v10_13), .iOS(.v11), .tvOS(.v11)],
     products: [.library(name: "AudioKit", targets: ["AudioKit"])],
+    dependencies: [.package(url: "https://github.com/orchetect/MIDIKit", from: "0.4.0")],
     targets: [
-        .target(name: "AudioKit"),
+        .target(name: "AudioKit", dependencies: ["MIDIKit"]),
         .testTarget(name: "AudioKitTests", dependencies: ["AudioKit"], resources: [.copy("TestResources/")]),
     ],
     cxxLanguageStandard: .cxx14
